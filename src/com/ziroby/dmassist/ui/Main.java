a44b7cf49b9b5131fd5874d628a1b92b47d210fb
ziroby <ziroby@users.sourceforge.net>
2009-10-22 18:57:29 -0500

    Changed version number to 1.3

--- src/com/ziroby/dmassist/ui/Main.java
+++ src/com/ziroby/dmassist/ui/Main.java
@@ -27,7 +27,7 @@ import javax.swing.JFrame;
  */
 public class Main {
 
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.3";
 
 
 	static void createAndShowGUI() {
