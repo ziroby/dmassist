357adf1ad0ac95136d74739b0535243bb237a106
ziroby <ziroby@users.sourceforge.net>
2009-10-28 12:13:29 -0500

    Changed to version 1.2 & removed versionb number from splash screen.

--- src/com/ziroby/dmassist/ui/Main.java
+++ src/com/ziroby/dmassist/ui/Main.java
@@ -27,7 +27,7 @@ import javax.swing.JFrame;
  */
 public class Main {
 
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.2";
 
 
 	static void createAndShowGUI() {
