2c3d95ef762b2e176a0a0d8cd251c0adacbdc380
ziroby <ziroby@users.sourceforge.net>
2010-05-31 10:17:31 -0500

    Added (Android) about box

---
+++ src/android/src/com/ziroby/android/dmassist/AboutBox.java
@@ -0,0 +1,13 @@
+package com.ziroby.android.dmassist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutBox extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_box);
+    }
+}
--- src/android/src/com/ziroby/android/dmassist/MainActivity.java
+++ src/android/src/com/ziroby/android/dmassist/MainActivity.java
@@ -8,10 +8,15 @@ import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ListAdapter;
@@ -28,6 +33,7 @@ public class MainActivity extends ListActivity {
     static final EntityList dataModel = new EntityListImpl();
     private static final int HIGHLIGHT_COLOR = Color.YELLOW;
     private static final int NON_HIGHLIGHT_COLOR = Color.WHITE;
+    private static final int MENU_ITEM_ABOUT = 0;
     private MyListAdapter listAdapter;
     private TextView initTextView;
     private TextView roundTextView;
@@ -127,12 +133,30 @@ public class MainActivity extends ListActivity {
                     handleresetButton();
                 }
             });
+
+            registerForContextMenu(getListView());
 }
         catch (Exception e) {
+            displayErrorDialog(e);
             Log.e(this.getClass().toString(), "Exception: " + e.getLocalizedMessage(), e);
         }
     }
     
+    private void displayErrorDialog(Exception e) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(e.getLocalizedMessage())
+               .setCancelable(false)
+               .setTitle("Error")
+               .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                   public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                   }
+               });
+        AlertDialog alert = builder.create();
+        
+        alert.show();
+    }
+
     protected void redraw() {
         try {            
             final Integer initCount = dataModel.getInitCount();
@@ -172,23 +196,6 @@ public class MainActivity extends ListActivity {
 //        getListView().getChildAt(1).setBackgroundColor(Color.YELLOW);
     }
     
-    @SuppressWarnings("unchecked")
-    private void showStupidDialog()
-    {
-        Map<String, String> item = (Map<String, String>) getListView().getItemAtPosition(0);
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage(item.getClass().toString())
-               .setCancelable(false)
-               .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                   }
-               });
-        AlertDialog alert = builder.create();
-        
-        alert.show();
-    }
-
     protected void handleNextButton() {
         dataModel.gotoNextInitCount();
     }
@@ -196,4 +203,57 @@ public class MainActivity extends ListActivity {
     protected void handleresetButton() {
         dataModel.resetNumRounds();
     }
+    
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        int position = 0;
+        menu.add(/* group id*/ 0, MENU_ITEM_ABOUT, position++,
+                R.string.displayAboutBox)
+                .setIcon(android.R.drawable.ic_menu_info_details);
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+            ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        int position = 0;
+        menu.add(/* group id*/ 0, MENU_ITEM_ABOUT, position++,
+                R.string.displayAboutBox);
+    }
+    
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        return onItemSelected(item);
+//        return super.onContextItemSelected(item);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return onItemSelected(item);
+    }
+    
+    private boolean onItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case MENU_ITEM_ABOUT:
+            displayAboutBox();
+            return true;
+        }
+        return false;
+    }
+
+    private void displayAboutBox() {
+        try {
+            Intent intent = new Intent(this, AboutBox.class);
+            startActivity(intent);
+        }
+        catch (Exception e) {
+            displayErrorDialog(e);
+            Log.e(this.getClass().toString(), "Exception: " + e.getLocalizedMessage(), e);
+        }
+
+    }
+    
 }
