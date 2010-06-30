54ffd0f445e50bce01dc99b2011655838a6689f3
ziroby <ziroby@users.sourceforge.net>
2010-06-29 21:57:00 -0500

    Highlighting: non-positive hit points, subdued, last round of an effect.

---
+++ src/android/src/com/ziroby/android/dmassist/EntityListAdapter.java
@@ -0,0 +1,131 @@
+/*
+ *   Copyright 2010 Ron "Ziroby" Romero
+ * 
+ *   This file is part of dmassist-android.
+ *
+ *   dmassist-android is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   dmassist-android is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with dmassist-android.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.ziroby.android.dmassist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.ziroby.dmassist.gwtable.model.Entity;
+import com.ziroby.dmassist.gwtable.model.EntityList;
+import com.ziroby.dmassist.gwtable.util.Listener;
+import com.ziroby.dmassist.gwtable.util.ObjectEvent;
+
+public class EntityListAdapter
+extends SimpleAdapter
+implements ListAdapter
+{
+    private static final int HIGHLIGHT_COLOR = Color.YELLOW;
+    private static final int NON_HIGHLIGHT_COLOR = Color.WHITE;
+
+    static List<Map<String, String>> data = new ArrayList<Map<String, String>>();
+
+    public EntityListAdapter(Context context, int resource)
+    {
+        super(context, MainActivity.dataModel.getListOfMaps(data), resource,
+                new String[] {
+            EntityList.COLUMN_NAME_NAME,
+            EntityList.COLUMN_NAME_INIT,
+            EntityList.COLUMN_NAME_HP,
+            EntityList.COLUMN_NAME_SUBDUAL,
+            EntityList.COLUMN_NAME_ROUNDS},
+            new int[] {R.id.name, R.id.init, R.id.hp, R.id.sub, R.id.rounds});
+
+        MainActivity.dataModel.addListener(new Listener() {
+            public void objectChanged(ObjectEvent event) {
+                MainActivity.dataModel.getListOfMaps(data);
+                notifyDataSetChanged();
+            }
+        });
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final View view = super.getView(position, convertView, parent);
+
+        Entity entity = MainActivity.dataModel.getEntity(position);
+        highlightEntity(entity, view);
+        return view;
+    }
+
+    private void highlightEntity(Entity entity, final View view) {
+
+        highlightIfCurrentTurn(entity, view);
+        highlightNegativeHitpoints(entity, view);
+        highlightSubdued(entity, view);
+        highlightExpired(entity, view);
+    }
+
+    private void highlightNegativeHitpoints(Entity entity, final View view) {
+        TextView hpView = (TextView) view.findViewById(R.id.hp);
+        if (hpView != null) {
+            final Integer hitpoints = entity.getHitpoints();
+            final boolean shouldHighlight = hitpoints != null && hitpoints < 1;
+            highlightText(hpView, shouldHighlight);
+        }
+    }
+
+    private void highlightSubdued(Entity entity, final View view) {
+        TextView subView = (TextView) view.findViewById(R.id.sub);
+        if (subView != null) {
+            final boolean shouldHighlight = entity.isSubdued();
+            highlightText(subView, shouldHighlight);
+        }
+    }
+
+    private void highlightExpired(Entity entity, final View view) {
+        TextView roundsView = (TextView) view.findViewById(R.id.rounds);
+        if (roundsView != null) {
+            final boolean shouldHighlight = entity.isExpired(0);
+            highlightText(roundsView, shouldHighlight);
+        }
+    }
+
+    private void highlightText(TextView view,
+            final boolean shouldHighlight) {
+        if (shouldHighlight) {
+            view.setTextColor(Color.BLUE);
+            view.setTypeface(Typeface.DEFAULT_BOLD);
+        }
+        else {
+            view.setTextColor(Color.BLACK);
+            view.setTypeface(Typeface.DEFAULT);
+        }
+    }
+
+    private void highlightIfCurrentTurn(Entity entity, final View view) {
+        final Integer initCount = MainActivity.dataModel.getInitCount();
+        final Integer initRoll = entity.getInitRoll();
+        if (initRoll != null && initRoll == initCount)
+            view.setBackgroundColor(HIGHLIGHT_COLOR);
+        else
+            view.setBackgroundColor(NON_HIGHLIGHT_COLOR);
+    }
+
+}
--- src/android/src/com/ziroby/android/dmassist/MainActivity.java
+++ src/android/src/com/ziroby/android/dmassist/MainActivity.java
@@ -1,16 +1,10 @@
 package com.ziroby.android.dmassist;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -21,8 +15,6 @@ import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 
@@ -39,9 +31,6 @@ public class MainActivity extends ListActivity {
 
     static final EntityList dataModel = new EntityListImpl();
 
-    private static final int HIGHLIGHT_COLOR = Color.YELLOW;
-    private static final int NON_HIGHLIGHT_COLOR = Color.WHITE;
-
     private static final int MENU_ITEM_ABOUT = 0;
     private static final int MENU_ITEM_ADD_CREATURE = 1;
     private static final int MENU_REMOVE = 2;
@@ -52,7 +41,7 @@ public class MainActivity extends ListActivity {
     private static final int REQUEST_CODE_ADD = 0;
 
 
-    private MyListAdapter listAdapter;
+    private EntityListAdapter listAdapter;
     private TextView initTextView;
     private TextView roundTextView;
     private TextView timeTextView;
@@ -60,66 +49,8 @@ public class MainActivity extends ListActivity {
     enum HealOrDamage {HEAL, DAMAGE}
     enum DamageOrSubdue {DAMAGE, SUBDUE}
 
-    private static void addSampleData(EntityList aDataModel) {
-        Entity row1 = new Entity();
-        row1.setAbbreviation("O"); //$NON-NLS-1$
-        row1.setName("Ogre"); //$NON-NLS-1$
-        row1.setInitRoll(12);
-        row1.setHitpoints(25);
-        aDataModel.addEntity(row1);
-
-        Entity row2 = new Entity();
-        row2.setAbbreviation("G1"); //$NON-NLS-1$
-        row2.setName("Goblin 1"); //$NON-NLS-1$
-        row2.setInitRoll(4);
-        row2.setHitpoints(8);
-        row2.setSubdual(3);
-        aDataModel.addEntity(row2);
-
-        Entity row4 = new Entity();
-        row4.setName("Melf's acid arrow"); //$NON-NLS-1$
-        row4.setInitRoll(4);
-        row4.setRoundsLeft(3);
-        aDataModel.addEntity(row4);
-
-        Entity row3 = new Entity();
-        row3.setAbbreviation("G2"); //$NON-NLS-1$
-        row3.setName("Goblin 2"); //$NON-NLS-1$
-        row3.setInitRoll(15);
-        row3.setHitpoints(3);
-        aDataModel.addEntity(row3);
-    }
-
-
     static {
-        addSampleData(dataModel);
-    }
-
-    private static class MyListAdapter
-    extends SimpleAdapter
-    implements ListAdapter
-    {
-        static List<Map<String, String>> data = new ArrayList<Map<String, String>>();
-
-        public MyListAdapter(Context context, int resource)
-        {
-            super(context, dataModel.getListOfMaps(data), resource,
-                    new String[] {
-                EntityList.COLUMN_NAME_NAME,
-                EntityList.COLUMN_NAME_INIT,
-                EntityList.COLUMN_NAME_HP,
-                EntityList.COLUMN_NAME_SUBDUAL,
-                EntityList.COLUMN_NAME_ROUNDS},
-                new int[] {R.id.name, R.id.init, R.id.hp, R.id.sub, R.id.rounds});
-
-            dataModel.addListener(new Listener() {
-                public void objectChanged(ObjectEvent event) {
-                    dataModel.getListOfMaps(data);
-                    notifyDataSetChanged();
-                }
-            });
-        }
-
+        dataModel.addSampleData();
     }
 
     /** Called when the activity is first created. */
@@ -128,7 +59,7 @@ public class MainActivity extends ListActivity {
         try {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.main);
-            listAdapter = new MyListAdapter(this, R.layout.initiative_item_row);
+            listAdapter = new EntityListAdapter(this, R.layout.initiative_item_row);
             setListAdapter(listAdapter);
 
             initTextView = (TextView) findViewById(R.id.initView);
@@ -141,19 +72,7 @@ public class MainActivity extends ListActivity {
                 }
             });
 
-            Button nextButton = (Button) findViewById(R.id.nextButton);
-            nextButton.setOnClickListener(new OnClickListener() {
-                public void onClick(View v) {
-                    handleNextButton();
-                }
-            });
-
-            Button resetButton = (Button) findViewById(R.id.resetButton);
-            resetButton.setOnClickListener(new OnClickListener() {
-                public void onClick(View v) {
-                    handleresetButton();
-                }
-            });
+            setButtonClickListeners();
 
             registerForContextMenu(getListView());
         }
@@ -161,6 +80,22 @@ public class MainActivity extends ListActivity {
             AndroidUtils.displayErrorDialog(this, e);
             Log.e(this.getClass().toString(), "Exception: " + e.getLocalizedMessage(), e);
         }
+    }
+
+    private void setButtonClickListeners() {
+        Button nextButton = (Button) findViewById(R.id.nextButton);
+        nextButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                handleNextButton();
+            }
+        });
+
+        Button resetButton = (Button) findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                handleresetButton();
+            }
+        });
     }
 
     protected void redraw() {
@@ -171,28 +106,13 @@ public class MainActivity extends ListActivity {
             timeTextView.setText(dataModel.formatRoundsAsTime());
 
             getListView().invalidate();
-
-            // Highlight the entities who's turn it is.
-            final Collection<Entity> entities = dataModel.getEntities();
-            int row = entities.size()-1;
-            for (Entity entity : entities)
-            {
-                final Integer initRoll = entity.getInitRoll();
-                final View child = getListView().getChildAt(row);
-                if (child == null)
-                    break;
-                if (initRoll != null && initRoll == initCount)
-                    child.setBackgroundColor(HIGHLIGHT_COLOR);
-                else
-                    child.setBackgroundColor(NON_HIGHLIGHT_COLOR);
-                --row;
-            }
         }
         catch (Throwable t)
         {
             AndroidUtils.displayErrorDialog(this, null);
         }
     }
+
 
     protected void handleNextButton() {
         dataModel.gotoNextInitCount();
@@ -238,7 +158,6 @@ public class MainActivity extends ListActivity {
         return onItemSelected(item);
     }
 
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return onItemSelected(item);
     }
@@ -420,6 +339,8 @@ public class MainActivity extends ListActivity {
                     entity.healSubdual(amount.roll());
                 else
                     entity.subdue(amount.roll());
+
+            redraw();
         }
         catch (Exception e) {
             AndroidUtils.displayErrorDialog(this, e);
