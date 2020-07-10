1e752a37000893232d59f06c5c67b9074588d9f7
Ron Ziroby Romero <ziroby@ziroby.com>
2020-07-10 21:30:22 +0100

    Added initial Spring Boot app

---
+++ src/server/src/main/java/com/ziroby/dmassist/server/DmAssistServerApplication.java
@@ -0,0 +1,13 @@
+package com.ziroby.dmassist.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmAssistServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmAssistServerApplication.class, args);
+	}
+
+}
---
+++ src/server/src/test/java/com/ziroby/dmassist/server/DmAssistServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziroby.dmassist.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmAssistServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
