47a19afd9508befc498e70456a8a64ba94ca8201
Ron Ziroby Romero <ziroby@ziroby.com>
2020-07-16 19:57:34 +0100

    DMA-17 Add a populate command

--- src/server/src/main/java/com/ziroby/dmassist/server/BattleController.java
+++ src/server/src/main/java/com/ziroby/dmassist/server/BattleController.java
@@ -2,30 +2,34 @@ package com.ziroby.dmassist.server;
 
 import com.ziroby.dmassist.gwtable.model.Entity;
 import com.ziroby.dmassist.gwtable.model.EntityList;
-import com.ziroby.dmassist.model.EntityListImpl;
+import com.ziroby.dmassist.model.EnhancedEntityList;
 import com.ziroby.dmassist.server.model.JsonEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-public class EntityController {
+public class BattleController {
 
-    private EntityList entityList = new EntityListImpl();
+    private EntityList entityList = new EnhancedEntityList();
 
     @RequestMapping("/battles/1/entities")
     @ResponseBody
-    public List<JsonEntity> index() {
+    public List<JsonEntity> entities() {
         Collection<Entity> entities = entityList.getEntities();
-        entities.stream().map(entity -> new JsonEntity(entity));
+        return entities.stream()
+                .map(JsonEntity::new)
+                .collect(Collectors.toList());
+    }
 
-
-        return Arrays.asList(JsonEntity.builder().name("ziroby").build());
+    @PostMapping("/battles/1/populate")
+    public void populate() {
+        entityList.addSampleData();
     }
 
 }
