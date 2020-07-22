d078d9ba9e0dd2a64ff262b445cdfccc2c4ee963
Ron Ziroby Romero <ziroby@ziroby.com>
2020-07-22 20:45:31 +0100

    DMA-23 Fix CORS

--- src/server/src/main/java/com/ziroby/dmassist/server/DmAssistServerApplication.java
+++ src/server/src/main/java/com/ziroby/dmassist/server/DmAssistServerApplication.java
@@ -2,6 +2,9 @@ package com.ziroby.dmassist.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class DmAssistServerApplication {
@@ -10,4 +13,14 @@ public class DmAssistServerApplication {
 		SpringApplication.run(DmAssistServerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://dmassist-frontend.herokuapp.com");
+			}
+		};
+	}
 }
